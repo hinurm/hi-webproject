bfb9374dec83ca958c673a3681ded4a28975b20e
hinurm <104059609+hinurm@users.noreply.github.com>
2022-04-24 18:50:15 +0300

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -38,9 +38,7 @@ return L.circleMarker(latlng, options)
 async function addCelltowersGeoJson(url) {
 const response = await fetch(url)
 const data = await response.json()
-const circles = L.geoJson(data, {
-  pointToLayer: createCircle,
-}
+const markers = L.geoJson(data)
 const clusters = L.markerClusterGroup()
 clusters.addLayer(markers)
 clusters.addTo(map))
